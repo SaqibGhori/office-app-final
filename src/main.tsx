4eff404ed71c9cc16c0604bf654138aaa0faca67
Dev4644 <dev4@oip.com.pk>
2025-11-01 03:52:37 +0500

    build fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './Style.css'
-import App from './App.tsx'
-import { ToastContainer } from "react-toastify";
+// import App from './App.tsx'
+// import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 
